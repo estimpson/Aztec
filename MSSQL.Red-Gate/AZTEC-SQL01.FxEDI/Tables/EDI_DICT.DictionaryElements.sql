9a57a0549c9131199684d4e13dbe892704b8e9fa
Eric Stimpson <estimpson@fore-thought.com>
2021-01-05 10:33:17 -0500

    Indexes for performance. Modify Toyota inbound 830 map for Toyota Sales location of ship to code.

--- MSSQL.Red-Gate/AZTEC-SQL01.FxEDI/Tables/EDI_DICT.DictionaryElements.sql
+++ MSSQL.Red-Gate/AZTEC-SQL01.FxEDI/Tables/EDI_DICT.DictionaryElements.sql
@@ -11,3 +11,7 @@ CREATE TABLE [EDI_DICT].[DictionaryElements]
 GO
 ALTER TABLE [EDI_DICT].[DictionaryElements] ADD CONSTRAINT [UQ__Dictiona__292205629BF0F73A] UNIQUE NONCLUSTERED  ([DictionaryVersion], [ElementCode]) ON [PRIMARY]
 GO
+CREATE NONCLUSTERED INDEX [ix_DE_1] ON [EDI_DICT].[DictionaryElements] ([DictionaryVersion], [ElementCode]) INCLUDE ([ElementDataType], [ElementName]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [ix_DE_2] ON [EDI_DICT].[DictionaryElements] ([ElementCode], [DictionaryVersion]) INCLUDE ([ElementDataType], [ElementName]) ON [PRIMARY]
+GO
